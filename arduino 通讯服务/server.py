2694d3902374fe5f6f8502bc1a96fc818c00f056
lucycore <32613263+lucycore@users.noreply.github.com>
2019-11-18 21:39:50 +0800

    Update server.py

--- arduino 通讯服务/server.py
+++ arduino 通讯服务/server.py
@@ -13,14 +13,24 @@ sock.bind((host, port))
 sock.listen(5)
 #对话循环
 
-def get_time(userlist):
-	zlis = []
-	for key, value in userlist.items():   
-		a = []
-		a.append(key)
-		a.append(value)
-		a='yicengfenli'.join(a)
-		zlis.append(a)
 
-	zlis='ercengfenli'.join(zlis)
-	return zlis
+while True:
+	try:
+		#堵塞连接
+		cli, addr = sock.accept()
+		print("\n\n客户端接入！")
+		print(addr)
+
+		#接收模式识别
+		mod = cli.recv(2048).decode()
+
+		#模式为验证更新
+		if mod == "SXY":
+			cli.sendall("3".encode())
+			js = 0
+			for x in range(3):
+				js += 1
+				a = cli.recv(2048).decode()
+				cli.sendall(str(js).encode())
+
+			
